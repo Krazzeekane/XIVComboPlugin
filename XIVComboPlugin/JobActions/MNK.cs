3102f5253e984953e73f72a3e54e305a52053f0b
Krazzeekane <kaneevans@gmail.com>
2022-01-20 14:41:14 -0800

    Update MNK.cs

--- XIVComboPlugin/JobActions/MNK.cs
+++ XIVComboPlugin/JobActions/MNK.cs
@@ -9,6 +9,9 @@ namespace XIVComboPlugin.JobActions
     public static class MNK
     {
         public const uint
+            Bootshine = 53,
+            TwinSnakes = 61,
+            SnapPunch = 56,
             AOTD = 62,
             FourPointFury = 16473,
             Rockbreaker = 70;
